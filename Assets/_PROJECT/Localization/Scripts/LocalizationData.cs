b9d01204a8dfa3ad2baee6379a8e0995e3cb2a9c
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-03-22 01:28:00 +0300

    Update Localization

---
+++ Assets/_PROJECT/Localization/Scripts/LocalizationData.cs
@@ -0,0 +1,8 @@
+using Architecture_M;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Architecture_M/Localization/Localization Data")]
+public class LocalizationData : LocalizationDataBase
+{
+
+}

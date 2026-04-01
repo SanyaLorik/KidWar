5f5c2f49e00c806287e3e400772a47f61cf33934
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-04-02 00:02:55 +0300

    Update view item roulette skin

--- Assets/_PROJECT/Ui/Roulette Skin/Scripts/RouletteSkin.cs
+++ Assets/_PROJECT/Ui/Roulette Skin/Scripts/RouletteSkin.cs
@@ -4,35 +4,34 @@ using UnityEngine;
 
 public class RouletteSkin : MonoBehaviour
 {
+    [Header("Parametrs")]
     [SerializeField] private int _spinCount;
     [SerializeField] private float _duration;
     [SerializeField] private AnimationCurve _behaviourCurve;
+
+    [Header("Views")]
+    [SerializeField] private RouletteSkinItem[] _items;
+
+    [Header("Pets")]
+    [SerializeField] private int TEST;
+
+    [Header("Other")]
     [SerializeField] private RectTransform _rect;
-    [SerializeField] private RectTransform[] _items;
 
     private void Start()
     {
         SpinAsync().Forget();
     }
 
-    private void ShiftArray()
-    {
-        var last = _items[^1];
-        for (int i = _items.Length - 1; i >= 1; i--)
-            _items[i] = _items[i - 1];
-
-        _items[0] = last;
-    }
-
-    private async UniTaskVoid SpinAsync()
+    public async UniTask SpinAsync()
     {
         float expendedTime = 0;
 
-        float spacing = _items[1].anchoredPosition.x - _items[0].anchoredPosition.x;
+        float spacing = _items[1].Rect.anchoredPosition.x - _items[0].Rect.anchoredPosition.x;
         float borderX = _rect.anchoredPosition.x + _rect.rect.width;
         float replaceX = borderX + spacing;
 
-        float distance = (_items[^1].anchoredPosition.x + spacing) - _items[0].anchoredPosition.x;
+        float distance = (_items[^1].Rect.anchoredPosition.x + spacing) - _items[0].Rect.anchoredPosition.x;
         float totalDistance = distance * _spinCount;
 
         print("distance " + distance);
@@ -46,15 +45,16 @@ public class RouletteSkin : MonoBehaviour
             float evaluated = _behaviourCurve.Evaluate(lerpTime);
             float currentDistance = Mathf.Lerp(0, totalDistance, evaluated);
             float xOffset = currentDistance - previousDistance;
+
             previousDistance = currentDistance;
 
             for (int i = 0; i < _items.Length; i++)
             {
-                _items[i].anchoredPosition = _items[i].anchoredPosition.AddX(xOffset);
+                _items[i].Rect.anchoredPosition = _items[i].Rect.anchoredPosition.AddX(xOffset);
 
-                if (replaceX < _items[i].anchoredPosition.x)
+                if (replaceX < _items[i].Rect.anchoredPosition.x)
                 {
-                    _items[i].anchoredPosition = _items[0].anchoredPosition.AddX(-spacing);
+                    _items[i].Rect.anchoredPosition = _items[0].Rect.anchoredPosition.AddX(-spacing);
                     ShiftArray();
                 }
             }
@@ -64,5 +64,14 @@ public class RouletteSkin : MonoBehaviour
             await UniTask.Yield(cancellationToken: destroyCancellationToken);
         }
         while (expendedTime < _duration);
+    }
+
+    private void ShiftArray()
+    {
+        var last = _items[^1];
+        for (int i = _items.Length - 1; i >= 1; i--)
+            _items[i] = _items[i - 1];
+
+        _items[0] = last;
     }
 }
---
+++ Assets/_PROJECT/Ui/Roulette Skin/Scripts/RouletteSkinItem.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RouletteSkinItem : MonoBehaviour
+{
+    [field: Header("Logic")]
+    [field: SerializeField] public RectTransform Rect { get; private set; }
+
+    [field: Header("View")]
+    [SerializeField] private Image _icon;
+    [SerializeField] private TextMeshProUGUI _damageText;
+
+    public void SetInfo(Sprite icon, int damage)
+    {
+        _icon.sprite = icon;
+        _damageText.text = damage.ToString();
+    }
+}

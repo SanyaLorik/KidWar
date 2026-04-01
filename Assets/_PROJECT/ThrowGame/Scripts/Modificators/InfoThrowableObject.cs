90a5d8bd311d892155da7da178c3db6df8324d60
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-04-02 01:04:36 +0300

    Add ions for prefabs

--- Assets/_PROJECT/ThrowGame/Scripts/Modificators/InfoThrowableObject.cs
+++ Assets/_PROJECT/ThrowGame/Scripts/Modificators/InfoThrowableObject.cs
@@ -6,5 +6,5 @@ public class InfoThrowableObject
 {
     [field: SerializeField] public Sprite Icon { get; private set; }
     [field: SerializeField][field: Min(0)] public int Damage { get; private set; }
-    [field: SerializeField][field: Range(0, 1)] public int DropChance { get; private set; }
+    [field: SerializeField][field: Range(0, 1)] public float DropChance { get; private set; }
 }

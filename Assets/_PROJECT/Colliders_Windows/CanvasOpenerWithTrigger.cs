ebc5102377ba8e8010634eece63dfd42c2f47976
ance1ad <artem_tvinkov@mail.ru>
2026-04-05 22:54:50 +0300

    add window opener with trigger

---
+++ Assets/_PROJECT/Colliders_Windows/CanvasOpenerWithTrigger.cs
@@ -0,0 +1,27 @@
+﻿using SanyaBeerExtension;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasOpenerWithTrigger : TriggerBehaviourBase {
+    [SerializeField] private DelayedTrigger _delayedTrigger;
+    [SerializeField] private GameObject _canvas;
+    [SerializeField] private Button _openButton;
+    [SerializeField] private Button _closeButton;
+
+    private void OnEnable() {
+        _openButton.onClick.AddListener(_canvas.ActiveSelf);
+        _closeButton.onClick.AddListener(_canvas.DisactiveSelf);
+    }
+
+    protected override void PlayerBehaviourOnEnter() {
+        _delayedTrigger.DelayedTriggerAction(TriggerAction); 
+    }
+    
+    protected override void PlayerBehaviourOnExit() {
+        _delayedTrigger.CancelTriggerAction();
+    }
+
+    private void TriggerAction() {
+        _canvas.ActiveSelf();
+    }
+}
